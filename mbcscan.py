488d0a6c285db1a0bef163819a97d6c808f4cbca
AccidentalRebel <accidentalrebel@gmail.com>
2021-03-29 08:44:57 +0800

    Can now select and print.

--- mbcscan.py
+++ mbcscan.py
@@ -5,66 +5,77 @@ from argparse import ArgumentParser
 
 g_args = None
 g_behaviors_list = None
-g_behavior_dict = {}
+g_behaviors_dict = {}
 
 def print_behaviors_list(behavior_list):
+    i = 0
     for behavior_external_id in behavior_list:
-        if behavior_external_id in g_behavior_dict.keys():
-            behavior = g_behavior_dict[behavior_external_id]
+        if behavior_external_id in g_behaviors_dict.keys():
+            behavior = g_behaviors_dict[behavior_external_id]
         else:
             behavior = get_behavior_by_external_id(src, behavior_external_id)
             if not behavior:
                 print('[ERROR] ExternalID ' + behavior_external_id + ' is not valid.')
                 raise SystemExit(1)
 
-            g_behavior_dict[behavior_external_id] = behavior
+            g_behaviors_dict[behavior_external_id] = behavior
 
-        print('behavior: ' + behavior.name + ' (' + behavior_external_id + ')');
+        print('[' + str(i) + '] ' + behavior.name + ' (' + behavior_external_id + ')');
+        i+=1
+
+def print_behavior_details(behavior):
+    if not behavior:
+        print('[ERROR] ExternalID ' + g_args.externalId + ' is not valid.')
+        raise SystemExit(1)
+
+    print('Behavior Details:\n' \
+          '=================\n' \
+          'Name:\t\t' + behavior.name + '\n' \
+          'MBC_ID:\t\t' + behavior.id)
+
+    if behavior.kill_chain_phases:
+        phase_str = 'Objectives:\t'
+        for phase in behavior.kill_chain_phases:
+            phase_shortname = phase.phase_name
+            obj = get_objective_by_shortname(src, phase_shortname)
+            if obj:
+                obj_eid = get_mbc_external_id(obj)
+                phase_str += obj.name + ' (' + obj_eid + ')'
+
+    if behavior.x_mitre_is_subtechnique:
+        parent = get_parent_behavior(src, behavior.id)
+        if parent:
+            parent_eid = get_mbc_external_id(parent)
+            print('Parent:\t\t' + parent.name + ' (' + parent_eid + ')')
+
+    print('\nDescription:\n' + behavior.description + '\n')
+
+    i = 0
+    if behavior.external_references:
+        print('External references:')
+        for ref in behavior.external_references:
+            if ref.url:
+                print('[' + str(i) + '] ' + ref.url)
+                i += 1
 
 class MBCScanShell(cmd.Cmd):
     intro = 'Intro. Type "?" for help.'
     prompt = '(prompt) '
 
-    def do_print_behaviors_list(self, arg):
+    def do_list(self, arg):
+        'Lists the determined behaviors on the file.'
         print_behaviors_list(g_behaviors_list)
+
+    def do_select(self, arg):
+        'Selects and displays the details of a particular behavior.'
+        selection_index = int(arg)
+        behavior = list(g_behaviors_dict.values())[selection_index]
+        print_behavior_details(behavior)
 
     def do_print_behavior(self, arg):
         'Prints behavior details'
-        behavior = get_behavior_by_external_id(src, g_args.externalid)
-        if not behavior:
-            print('[ERROR] ExternalID ' + g_args.externalId + ' is not valid.')
-            raise SystemExit(1)
-
-        print('Behavior Details:\n' \
-              '=================\n' \
-              'Name:\t\t' + behavior.name + '\n' \
-              'MBC_ID:\t\t' + behavior.id)
-
-        if behavior.kill_chain_phases:
-            phase_str = 'Objectives:\t'
-            for phase in behavior.kill_chain_phases:
-                phase_shortname = phase.phase_name
-                obj = get_objective_by_shortname(src, phase_shortname)
-                if obj:
-                    obj_eid = get_mbc_external_id(obj)
-                    phase_str += obj.name + ' (' + obj_eid + ')'
-
-        if behavior.x_mitre_is_subtechnique:
-            parent = get_parent_behavior(src, behavior.id)
-            if parent:
-                parent_eid = get_mbc_external_id(parent)
-                print('Parent:\t\t' + parent.name + ' (' + parent_eid + ')')
-
-        print('\nDescription:\n' + behavior.description + '\n')
-
-        i = 0
-        if behavior.external_references:
-            print('External references:')
-            for ref in behavior.external_references:
-                if ref.url:
-                    print('[' + str(i) + '] ' + ref.url)
-                    i += 1
-
+        print_behavior_details()
+        
     def do_exit(self, arg):
         'Exits the program'
         return True
