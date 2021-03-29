570f4c55160ebe2ccec904021228166f5e057281
AccidentalRebel <accidentalrebel@gmail.com>
2021-03-29 16:41:41 +0800

    Renamed to obj.

--- mbcscan.py
+++ mbcscan.py
@@ -53,19 +53,19 @@ def print_behaviors_list(behavior_list, can_show_all=False):
             
         i+=1
 
-def print_obj_details(behavior):
-    if not behavior:
-        print('[ERROR] Behavior not provided.')
+def print_obj_details(obj):
+    if not obj:
+        print('[ERROR] Obj not provided.')
         raise SystemExit(1)
 
     print(('=' * 80) + '\n' \
-          'Name:\t\t' + behavior.name + '\n' \
+          'Name:\t\t' + obj.name + '\n' \
           + ('=' * 80) + '\n' \
-          'MBC_ID:\t\t' + behavior.id)
+          'MBC_ID:\t\t' + obj.id)
 
-    if hasattr(behavior, 'kill_chain_phases'):
+    if hasattr(obj, 'kill_chain_phases'):
         phase_str = 'Objectives:\t'
-        for phase in behavior.kill_chain_phases:
+        for phase in obj.kill_chain_phases:
             phase_shortname = phase.phase_name
             obj = get_objective_by_shortname(g_src, phase_shortname)
             if obj:
@@ -73,8 +73,8 @@ def print_obj_details(behavior):
                 phase_str += obj.name + ' (' + obj_eid + ')'
 
     s = 'Parent:\t\t'
-    if hasattr(behavior, 'x_mitre_is_subtechnique'):
-        parent = get_parent_behavior(g_src, behavior.id)
+    if hasattr(obj, 'x_mitre_is_subtechnique'):
+        parent = get_parent_obj(g_src, obj.id)
         if parent:
             parent_eid = get_mbc_external_id(parent)
             s += parent.name + ' (' + parent_eid + ')'
@@ -83,7 +83,7 @@ def print_obj_details(behavior):
     print(s)
 
     s = 'Samples:\t'                
-    malwares = get_malwares_using_behavior(g_src, behavior.id)
+    malwares = get_malwares_using_behavior(g_src, obj.id)
     if malwares:
         i = 0
         for m in malwares:
@@ -96,11 +96,11 @@ def print_obj_details(behavior):
         s += 'None'
     print(s)
 
-    print('\nDescription:\t' + behavior.description + '\n')
+    print('\nDescription:\t' + obj.description + '\n')
 
-    if behavior.external_references:
+    if obj.external_references:
         print('External references:')
-        for ref in behavior.external_references:
+        for ref in obj.external_references:
             if ref.url:
                 print('- ' + ref.url)
 
@@ -117,9 +117,13 @@ class MBCScanShell(cmd.Cmd):
             can_show_all = True
         print_behaviors_list(g_behaviors_list, can_show_all)
 
-    def do_ls(self, arg):
+    def do_l(self, arg):
         'Lists the determined behaviors on the file.'
-        self.do_list(arg)
+        self.do_list(None)
+
+    def do_a(self, arg):
+        'Lists all the determined behaviors on the file.'
+        self.do_list('all')
 
     def do_select(self, arg):
         'Selects and displays the details of a particular behavior.'
